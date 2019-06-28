9df3d350c1cd416c0e51fd6bc1c6b580641c80b8
messengerW <messenger232624@gmail.com>
2019-06-28 23:02:59 +0800

    form

--- src/main/java/msw/controller/DemoController.java
+++ src/main/java/msw/controller/DemoController.java
@@ -1,14 +1,18 @@
 package msw.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/home")
 public class DemoController {
 
-    @RequestMapping("/index")
-    public String index(){
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String index(ModelMap modelMap ){
+        modelMap.addAttribute("msg","i am wzx");
         return "index";
     }
 }
--- target/demo/views/index.jsp
+++ target/demo/views/index.jsp
@@ -1,5 +1,8 @@
-<html>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %><html>
 <body>
-<h2>Hello World!</h2>
+    <h2>Hello World!</h2>
+    <h2>${msg}</h2>
 </body>
 </html>
